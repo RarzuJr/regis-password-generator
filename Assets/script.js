3b5a2cd4ef88147549b709e9f0ff9ecac526c1a8
Regis Arzu <rarzujr@gmail.com>
2021-01-17 17:14:11 -0700

    1.17.21 5PM Update

--- Assets/script.js
+++ Assets/script.js
@@ -5,24 +5,24 @@ var validNumber;
 var validCharacters;
 var lettersUppercase;
 var letersLowercase;
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 SpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var SmallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //var UpperLetters = [SmallLetters.map(toUpperCase)];
 
 // Write password to the #password input
 function writePassword() {
+  gather_character_count()
+  gather_character_attribute()
+  determine_choices()
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-
-
 
 //When A clicks on the generate button I ask the user specific questions to help me generate the password.
 function generatePassword() {
@@ -31,72 +31,79 @@ function generatePassword() {
 
 
 
+function gather_character_count() {
+  // I ask the user the questions from the criiterias 
+  // 1. ask the user for the length of the pasword 
 
-// I ask the user the questions from the criiterias 
+  var character_count = prompt("Please choose any character length between 8 and 128",);
 
-// 1. ask the user for the length of the pasword 
-if (!okay) {
-  alert("Ready for me to generate a password for you?");
-} else if (okay < 8 || okay > 128) {
-  okay = parseInt(prompt("You must choose between 8 and 128"));
+  if (character_count < 8 || character_count > 128) {
+    gather_criteria()
+  }
+}
+
+
+function gather_character_attribute() {
+  // 2. ask the user if they want to use Uppercase
+  //3. ask the user if they want to use numeric
+  //4. ask the user if they want to use special charecter
+
+  validmNumber = confirm("Will this contain numbers?");
+  validCharacters = confirm("Will this contain special characters?");
+  lettersUppercase = confirm("Will this contain Uppercase letters?");
+  lettersLowercase = confirm("Will this contain Lowercase letters?");
+}
+
+function determine_choices() {
+
+  if (!validCharacters && !validNumber && !lettersUppercase && !lettersLowercase) {
+    choices = alert("please click on generate password");
+  }
+
+  else if (validCharacters && validmNumber && lettersUppercase) {
+    choices = SpecialCharacters.concat(numbers, UpperLetters);
+  }
+  else if (validCharacters && validmNumber && lettersLowercase) {
+    choices = SpecialCharacters.concat(numbers, SmallLetters);
+  }
+  else if (validCharacters && confirmLowercase && lettersUppercase) {
+    choices = SpecialCharacters.concat(SmallLetters, UpperLetters);
+  }
+  else if (validCharacters && lettersLowercase && lettersUppercase) {
+    choices = numbers.concat(SmallLetters, UpperLetters);
+  }
+  else if (validCharacters && validmNumber) {
+    choices = SpecialCharacters.concat(numbers);
+
+  } else if (validCharacters && lettersLowercase) {
+    choices = SpecialCharacters.concat(SmallLetters);
+
+  } else if (validCharacters && lettersUppercase) {
+    choices = SpecialCharacters.concat(UpperLetters);
+  }
+  else if (confirmLowercase && validmNumber) {
+    choices = SmallLetters.concat(numbers);
+
+  } else if (confirmLowercase && lettersUppercase) {
+    choices = SmallLetters.concat(UpperLetters);
+
+  } else if (validmNumber && lettersUppercase) {
+    choices = numbers.concat(UpperLetters);
+  }
+  else if (validCharacters) {
+    choices = SpecialCharacters;
+  }
+  else if (validmNumber) {
+    choices = numbers;
+  }
+  else if (lettersLowercase) {
+    choices = SmallLetters;
+  }
+  else if (lettersUppercase) {
+    choices = space.concat(UpperLetters);
+  };
 
 }
-// 2. ask the user if they want to use Uppercase
-//3. ask the user if they want to use numeric
-//4. ask the user if they want to use special charecter
-else {
-
-  var validmNumber = confirm("Will this contain numbers?");
-  var validCharacters = confirm("Will this contain special characters?");
-  var lettersUppercase = confirm("Will this contain Uppercase letters?");
-  var lettersLowercase = confirm("Will this contain Lowercase letters?");
-};
-if (!validCharacters && !validNumber && !lettersUppercase && !lettersLowercase) {
-  choices = alert("please choose a criteria!");
-}
-// Else if for 3 positive options
-else if (validCharacters && validmNumber && lettersUppercase) {
-  choices = SpecialCharacters.concat(numbers, UpperLetters);
-}
-else if (validCharacters && validmNumber && lettersLowercase) {
-  choices = SpecialCharacters.concat(numbers, SmallLetters);
-}
-else if (validCharacters && confirmLowercase && lettersUppercase) {
-  choices = SpecialCharacters.concat(SmallLetters, UpperLetters);
-}
-else if (validCharacters && lettersLowercase && lettersUppercase) {
-  choices = numbers.concat(SmallLetters, UpperLetters);
-}
-else if (validCharacters && validmNumber) {
-  choices = SpecialCharacters.concat(numbers);
-
-} else if (validCharacters && lettersLowercase) {
-  choices = SpecialCharacters.concat(SmallLetters);
-
-} else if (validCharacters && lettersUppercase) {
-  choices = SpecialCharacters.concat(UpperLetters);
-}
-else if (confirmLowercase && validmNumber) {
-  choices = SmallLetters.concat(numbers);
-
-} else if (confirmLowercase && lettersUppercase) {
-  choices = SmallLetters.concat(UpperLetters);
-
-} else if (validmNumber && lettersUppercase) {
-  choices = numbers.concat(UpperLetters);
-}
-else if (validCharacters) {
-  choices = SpecialCharacters;
-}
-else if (validmNumber) {
-  choices = numbers;
-}
-else if (lettersLowercase) {
-  choices = SmallLetters;
-}
-else if (lettersUppercase) {
-  choices = space.concat(UpperLetters);
-};
 
 //When the user answer the question it should validate evevrything they want
 // When all the questions are answered then all the password is generated correctly
@@ -122,4 +129,5 @@ else if (lettersUppercase) {
 // ## Mock-Up
 
 // The following image shows the web application's appearance and functionality:
+
 

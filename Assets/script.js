e48b8999830ac71162ad0b597d0ea3f159e138e5
Regis Arzu <rarzujr@gmail.com>
2021-02-02 14:57:48 -0700

    2.22.21 update

--- Assets/script.js
+++ Assets/script.js
@@ -1,133 +1,145 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var okay
-var validNumber;
-var validCharacters;
-var lettersUppercase;
-var letersLowercase;
+
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-SpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var SpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var SmallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//var UpperLetters = [SmallLetters.map(toUpperCase)];
+var UpperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Write password to the #password input
+
+funtion generateQuestions() {
+
+  var length = passwordLength(
+    prompt("How many charechters between 8-129 do you want your passsowrd to be ?");
+  );
+
+  if (isNaN(length) === true) {
+    alert("please provide password leagth");
+    return;
+  }
+  if (length < 8) {
+    alert('password must be a minimum of 8 charecters');
+    return;
+  }
+  if (length > 128) {
+    alert('password must be a maximum of 128 charecters');
+    return;
+  }
+  var SpecialCharacters = confirm(
+    'click ok to add special charecter'
+  );
+  var numbers = confirm(
+    'click ok to add numbers'
+  );
+  var UpperLetters = confirm(
+    'click ok to add capital letters'
+  );
+  var SmallLetters = confirm(
+    'click ok to add lowercase letters'
+  );
+
+  if (
+    SpecialCharacters === false &&,
+    numbers === false &&,
+    UpperLetters === false &&,
+    SmallLetters === false &&,
+) {
+    alert("please choose a charecter tpe")
+    return;
+  };
+  var allPossibleCharecters = {
+    length: length,
+    SpecialCharacters: SpecialCharacters,
+    numbers: numbers,
+    SmallLetters: SmallLetters,
+  };
+  return allPossibleCharecters;
+}
+
+function randomGen(array) {
+  var randomCharecters = Math.floor(Math.random() * array.length);
+  var randomPassword = arr[randomCharecters];
+
+  return randomPassword;
+}
+funtion generatePassword() {
+  var potentOptions = generateQuestions()
+  var results = [];
+  var potentialCharacters = []
+  var pickedCharecters = []
+
+}
+if (potentOptions.specialCharacters) {
+  potentialCharacters = potentialCharacters.concat(specialCharacters);
+  pickedCharecters.push(randomGen(specialCharacters));
+}
+
+
+if (potentOptions.numbers) {
+  potentialCharacters = potentialCharacters.concat(numbers);
+  pickedCharecters.push(randomGen(numbers));
+}
+
+if (potentOptions.hasLowerCasedCharacters) {
+  potentialCharacters = potentialCharacters.concat(SmallLetters);
+  pickedCharecters.push(randomGen(SmallLetters));
+}
+
+if (potentOptions.hasUpperCasedCharacters) {
+  potentialCharacters = potentialCharacters.concat(UpperLetters);
+  pickedCharecters.push(randomGen(UpperLetters));
+}
+for (var i = 0; i < pickedCharecters.length; i++) {
+  result[i] = pickedCharecters[i];
+}
+return result.join("");
+var generateBtn = document.querySelector('#generate');
+
 function writePassword() {
-  gather_character_count()
-  gather_character_attribute()
-  determine_choices()
-
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
+
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
 
-//When A clicks on the generate button I ask the user specific questions to help me generate the password.
-function generatePassword() {
-  okay = parseInt(prompt("How many characters would you like for your password ? Choose between 8 and 128"))
-}
+generateBtn.addEventListener('click', writePassword);
 
 
-
-function gather_character_count() {
-  // I ask the user the questions from the criiterias 
-  // 1. ask the user for the length of the pasword 
-
-  var character_count = prompt("Please choose any character length between 8 and 128",);
-
-  if (character_count < 8 || character_count > 128) {
-    gather_criteria()
-  }
-}
-
-
-function gather_character_attribute() {
-  // 2. ask the user if they want to use Uppercase
-  //3. ask the user if they want to use numeric
-  //4. ask the user if they want to use special charecter
-
-  validmNumber = confirm("Will this contain numbers?");
-  validCharacters = confirm("Will this contain special characters?");
-  lettersUppercase = confirm("Will this contain Uppercase letters?");
-  lettersLowercase = confirm("Will this contain Lowercase letters?");
-}
-
-function determine_choices() {
-
-  if (!validCharacters && !validNumber && !lettersUppercase && !lettersLowercase) {
-    choices = alert("please click on generate password");
-  }
-
-  else if (validCharacters && validmNumber && lettersUppercase) {
-    choices = SpecialCharacters.concat(numbers, UpperLetters);
-  }
-  else if (validCharacters && validmNumber && lettersLowercase) {
-    choices = SpecialCharacters.concat(numbers, SmallLetters);
-  }
-  else if (validCharacters && confirmLowercase && lettersUppercase) {
-    choices = SpecialCharacters.concat(SmallLetters, UpperLetters);
-  }
-  else if (validCharacters && lettersLowercase && lettersUppercase) {
-    choices = numbers.concat(SmallLetters, UpperLetters);
-  }
-  else if (validCharacters && validmNumber) {
-    choices = SpecialCharacters.concat(numbers);
-
-  } else if (validCharacters && lettersLowercase) {
-    choices = SpecialCharacters.concat(SmallLetters);
-
-  } else if (validCharacters && lettersUppercase) {
-    choices = SpecialCharacters.concat(UpperLetters);
-  }
-  else if (confirmLowercase && validmNumber) {
-    choices = SmallLetters.concat(numbers);
-
-  } else if (confirmLowercase && lettersUppercase) {
-    choices = SmallLetters.concat(UpperLetters);
-
-  } else if (validmNumber && lettersUppercase) {
-    choices = numbers.concat(UpperLetters);
-  }
-  else if (validCharacters) {
-    choices = SpecialCharacters;
-  }
-  else if (validmNumber) {
-    choices = numbers;
-  }
-  else if (lettersLowercase) {
-    choices = SmallLetters;
-  }
-  else if (lettersUppercase) {
-    choices = space.concat(UpperLetters);
-  };
-
-}
-
-//When the user answer the question it should validate evevrything they want
-// When all the questions are answered then all the password is generated correctly
-// When a password is generated it is displayed ont he screen
-// `
-``
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
+//* When I click on the "Password Geberator Button" an alert comes up to 
+//ask a series of questions
+// var password = {
+// document.getElementById("myBtngenerate").addEventListener("click", password);
+
+
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
+//*Depending on the answers to the question, 
+//determin what criteria to use in the passowrd
+//==>
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
+//*When asked about the length of the charecters it can be more than 8 less than 128
+
+
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
+//*When asked about the other stuff the password will pull from that criteria
+//==>
+
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
+//Passsword should have at least one of the charaters approved.
+
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
+//*When all props are answer the password is displayed in the  generator
+
+
 // THEN the password is either displayed in an alert or written to the page
 
 // ## Mock-Up
 
 // The following image shows the web application's appearance and functionality:
-
-
